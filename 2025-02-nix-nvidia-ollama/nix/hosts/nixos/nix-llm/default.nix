d14781a73e8207a408a14bf7ae5635a60202f4fd
John Poe <61294046+johnpoe007@users.noreply.github.com>
2025-04-25 10:49:11 -0400

    Update ip

--- 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -21,12 +21,12 @@
     interfaces.ens18 = {
       useDHCP = false;
       ipv4.addresses = [{
-        address = "10.42.37.100";
+        address = "192.168.7.153";
         prefixLength = 24;
       }];
     };
-    defaultGateway = "10.42.37.254";
-    nameservers = [ "10.42.37.254" ];
+    defaultGateway = "192.168.7.246";
+    nameservers = [ "192.168.7.246" ];
   };
 
   # System localization
